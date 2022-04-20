d04a47f384531b26b3f1b0796cab66f2cb1839f0
isabeels <101607831+isabeels@users.noreply.github.com>
2022-04-20 16:30:37 -0300

    Resolvendo merge

--- src/br/com/residencia/poo/contas/ContaCorrente.java
+++ src/br/com/residencia/poo/contas/ContaCorrente.java
@@ -1,10 +1,6 @@
 package br.com.residencia.poo.contas;
 
-<<<<<<< HEAD
-//import java.time.LocalDate;
-=======
 // import java.time.LocalDate;
->>>>>>> branch 'main' of https://github.com/isabeels/poo.git
 
 public class ContaCorrente {
 	Integer idContaCorrente;
