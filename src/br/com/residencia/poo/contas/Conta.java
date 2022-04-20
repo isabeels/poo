063c2b5db93f540e3fc8aacb1bbd74ec059d795d
KyuaKun <vinivsp2304@hotmail.com>
2022-04-20 16:14:06 -0300

    adcionado: getters, setters e construtor na classe conta

--- src/br/com/residencia/poo/contas/Conta.java
+++ src/br/com/residencia/poo/contas/Conta.java
@@ -3,13 +3,69 @@ package br.com.residencia.poo.contas;
 import java.time.LocalDate;
 
 public class Conta {
-	Integer idConta;
-	String numeroAgencia;
-	String tipoConta;
-	String numeroConta;
-	Double saldo;
-	LocalDate dataAbertura;
-	Boolean status;
-	String senha;
-	String qualquercoisa;
+	private Integer idConta;
+	private String numeroAgencia;
+	private String tipoConta;
+	private String numeroConta;
+	private Double saldo;
+	private LocalDate dataAbertura;
+	private Boolean status;
+	private String senha;
+	
+	public Conta(Integer idConta, String numeroAgencia, String tipoConta, String numeroConta, Double saldo,
+			LocalDate dataAbertura, Boolean status, String senha) {
+		
+		this.idConta = idConta;
+		this.numeroAgencia = numeroAgencia;
+		this.tipoConta = tipoConta;
+		this.numeroConta = numeroConta;
+		this.saldo = saldo;
+		this.dataAbertura = dataAbertura;
+		this.status = status;
+		this.senha = senha;
+	}
+
+
+	public Integer getIdConta() {
+		return idConta;
+	}
+	public String getNumeroAgencia() {
+		return numeroAgencia;
+	}
+	public String getTipoConta() {
+		return tipoConta;
+	}
+	public String getNumeroConta() {
+		return numeroConta;
+	}
+	public Double getSaldo() {
+		return saldo;
+	}
+	public LocalDate getDataAbertura() {
+		return dataAbertura;
+	}
+	public Boolean getStatus() {
+		return status;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setNumeroAgencia(String numeroAgencia) {
+		this.numeroAgencia = numeroAgencia;
+	}
+	public void setTipoConta(String tipoConta) {
+		this.tipoConta = tipoConta;
+	}
+	public void setNumeroConta(String numeroConta) {
+		this.numeroConta = numeroConta;
+	}
+	public void setStatus(Boolean status) {
+		this.status = status;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	
+	
 }
---
+++ src/br/com/residencia/poo/principal/Principal.java
@@ -0,0 +1,13 @@
+package br.com.residencia.poo.principal;
+
+import br.com.residencia.poo.contas.Conta;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Conta conta1 = new Conta(001, "3761", "Conta Corrente", "89121-5", 0.1, null , true, "123");
+		
+		
+	}
+
+}
