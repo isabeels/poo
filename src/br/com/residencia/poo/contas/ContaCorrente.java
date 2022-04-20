446dd52c7d2f74c824d5a7422c2074517c017194
isabeels <101607831+isabeels@users.noreply.github.com>
2022-04-20 16:25:10 -0300

    Pequena Correção

--- src/br/com/residencia/poo/contas/ContaCorrente.java
+++ src/br/com/residencia/poo/contas/ContaCorrente.java
@@ -1,13 +1,9 @@
 package br.com.residencia.poo.contas;
 
-import java.time.LocalDate;
+//import java.time.LocalDate;
 
 public class ContaCorrente {
 	Integer idContaCorrente;
 	Double chequeEspecial;
 	Double taxa;
-	
-	
-	
-
 }
