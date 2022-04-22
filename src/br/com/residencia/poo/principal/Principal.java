e1d951715c34b5b5c674b130457ab246cb710284
Gabriel <gabrieltsf10@gmail.com>
2022-04-22 17:01:29 -0300

    Adicionando comentário | Teste commit

--- src/br/com/residencia/poo/principal/Principal.java
+++ src/br/com/residencia/poo/principal/Principal.java
@@ -6,6 +6,8 @@ import br.com.residencia.poo.pessoas.Gerente;
 public class Principal {
 
 	public static void main(String[] args) {
+		
+		//Instanciação de contas
 
 		Conta conta1 = new Conta(1, "3761", "Conta Corrente", "89121-5", 0.15, true, "123");
 		System.out.println(conta1.toString());
@@ -13,7 +15,7 @@ public class Principal {
 		Conta conta2 = new Conta(2, "1673", "Conta Corrente", "51219-8", 1562.00, true, "321");
 		System.out.println(conta2.toString());
 
-
+		
 	}
 
 }
