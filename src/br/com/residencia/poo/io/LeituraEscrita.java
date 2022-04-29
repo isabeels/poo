f8a6dcd2174a855978ace9b0c11685109b3b470a
isabeels <101607831+isabeels@users.noreply.github.com>
2022-04-28 23:45:04 -0300

    Criando classe LeituraEscrita

--- src/br/com/residencia/poo/io/LeituraEscrita.java
+++ src/br/com/residencia/poo/io/LeituraEscrita.java
@@ -1,51 +1,51 @@
 package br.com.residencia.poo.io;
 
-import static java.lang.Integer.*;
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.Writer;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import br.com.residencia.poo.contas.Conta;
-import br.com.residencia.poo.contas.ContaCorrente;
-import br.com.residencia.poo.contas.ContaPoupanca;
 
 public class LeituraEscrita {
-	public static void main(String[] args) {
-	try (BufferedReader br = new BufferedReader(new FileReader("./temp/teste.txt"))) {
-        String line = br.readLine();
-        while (line != null) {
-        	StringBuilder output = new StringBuilder();
-        	String[] pieces = line.split("\\;");
-        	
-        	for(String piece : pieces) {
-        		List pow = new ArrayList();
-        		//System.out.println(piece);
-        		String [] doublePieces = piece.split("\\,");
-        		for(String doublePiece : doublePieces) {
-        			//System.out.println(doublePiece);
-        			if(doublePiece.equals("Corrente")||doublePiece.equals("Poupanca")) {
-        				pow.add(doublePiece);
-        			}else {
-        				int x = parseInt(doublePiece);
-        				pow.add(doublePiece);
-        			}
-        		}
-        		if(pow.get(0).toString() != null && pow.get(0).toString().equals("Corrente")) {
-        			ContaCorrente newC = new ContaCorrente(pow.get(1).toString(),pow.get(2).toString());
-        			System.out.println(newC);
-        		}else if(pow.get(0).toString()!=null && pow.get(0).toString().equals("Poupanca")) {
-        			Conta newP = new ContaPoupanca(pow.get(1).toString(),pow.get(2).toString());
-        			System.out.println(newP);
-        		}
-        		
-        		
-        	line = br.readLine();
-        }
-    }} catch (IOException e) {
-        System.out.println("Error: " + e.getMessage());
-    }
-}
+
+//	private static final String PATH_BASICO = "./temp/";
+//	private static final String EXTENSAO = "comprovantesSaque.txt";
+
+	public static void comprovanteSaque(Conta conta, double valorSaque) throws IOException {
+	
+	String nomeArquivo = conta.getCpf() + "_" + conta.getNumeroAgencia() + "_" + conta.getNumeroConta() + " transacoes";
+	
+	BufferedWriter bffWrite = new BufferedWriter(new FileWriter("./temp/comprovantesSaque.txt"));
+			
+	String linha = "จจจจจจจจจจจจ COMPROVANTE DE SAQUE จจจจจจจจจจจจ";
+	Writer buffWrite;
+	buffWrite.append(linha + "\n");
+	
+	linha =  "CPF" + conta.getCpf();
+	buffWrite.append(linha + "\n");
+
+	linha =  "Ag๊ncia" + conta.getNumeroAgencia();
+	buffWrite.append(linha + "\n");
+
+	linha =  "Conta" + conta.getNumeroConta();
+	buffWrite.append(linha + "\n");
+
+	linha =  "Valor; R$" + valorSaque;
+	buffWrite.append(linha + "\n");
+
+	SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy  HH:MM:ss");
+	Date date = new Date();
+	buffWrite.append("Opera็ใo realizada em: " + simpleDateFormat.format(date));
+
+	linha = "จจจจจจจจจจจจ VOLTE SEMPRE จจจจจจจจจจจจ";
+	buffWrite.append(linha + "\n\n");
+
+	buffWrite.close();
+	
+	}
 
 }
